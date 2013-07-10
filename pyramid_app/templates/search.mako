09cfb191dc74cdd633f56aa3174612ab0e12fa4a
pkrysiak <pawelkrysiak90@gmail.com>
2013-07-10 14:35:02 +0200

    corrects

--- pyramid_app/templates/search.mako
+++ pyramid_app/templates/search.mako
@@ -5,8 +5,8 @@
     <div class="main_box_left">
         <div class="name_product">
             Product name: ${product_name}
-            <p style = "margin-top:15px"><a href = "${allegro_link}"> ${allegro_link} </a></p>
-            <p style = "margin-top:15px"><a href = "${nokaut_link}"> ${nokaut_link} </a></p>
+            <p style = "margin-top:15px"><a href = "${'#' if allegro_link is None else allegro_link}"> ${allegro_link} </a></p>
+            <p style = "margin-top:15px"><a href = "${'#' if allegro_link is None else allegro_link}"> ${nokaut_link} </a></p>
         </div>
         <div class="box_photo">
             <p style="margin-top:250px"> <br></p>
@@ -16,11 +16,11 @@
     <div class="main_box_right">
         <div class="compare_box">
             <img src="${request.static_url('pyramid_app:static/img/logo_allegro.png')}" alt="logo_allegro"/>
-            <div class="${allegro_price_mode}">${allegro_price}</div>
+            <div class="${'price '+ allegro_price_mode}">${allegro_price}</div>
         </div>
         <div class="compare_box">
             <img src="${request.static_url('pyramid_app:static/img/logo_nokaut.png')}" alt="logo_nokaut"/>
-            <div class="${nokaut_price_mode}">${nokaut_price}</div>
+            <div class="${'price '+ nokaut_price_mode}">${nokaut_price}</div>
         </div>
     </div>
 </%block>
--- pyramid_app/views.py
+++ pyramid_app/views.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
-from allegro.lib import allegro_api,NoItemException
-from nokaut.lib import nokaut_api
+from pyramid.httpexceptions import HTTPNotFound
+from allegro.lib import allegro_api, NoItemException as AllegroNoItemEx
+from nokaut.lib import nokaut_api, NoItemException as NokautNoItemEx
 
 @view_config(route_name='home', renderer='pyramid_app:templates/base.mako')
 def my_view(request):
@@ -10,37 +10,43 @@ def my_view(request):
 @view_config(route_name = 'search', renderer = 'pyramid_app:templates/search.mako')
 def res_view(request):
     search_phrase = request.GET.get('search_field')
-    (all_link, all_price), (nok_link, nok_price), mode = _get_prices(search_phrase)
+    nokaut_key = request.registry.settings.get('nokaut.key')
 
-    if not mode:
-        return HTTPFound('/not_found')
+    try :
+        all = allegro_api(search_phrase)
+    except AllegroNoItemEx:
+        all = (None, None)
+
+    try:
+        nok = nokaut_api(search_phrase,nokaut_key)
+    except NokautNoItemEx:
+        nok = (None, None)
+
+
+    nok_price, nok_link = nok[1], nok[0]
+    all_price, all_link = all[1], all[0]
+
+    if nok_price == None and all_price == None:
+        all_mode, nok_mode = '', ''
+    elif nok_price != None and all_price == None:
+        all_mode, nok_mode = '', 'win'
+    elif nok_price == None and all_price != None:
+        all_mode, nok_mode = 'win', ''
+    else :
+        if all_price < nok_price:
+            all_mode, nok_mode = 'win', ''
+        else:
+            all_mode, nok_mode = '', 'win'
+
+    if all_mode == '' and nok_mode == '':
+        return HTTPNotFound('No items found..')
     else:
-        return {'product_name' : search_phrase,
+        return {
+                'product_name' : search_phrase,
                 'allegro_link' : all_link,
                 'nokaut_link' : nok_link,
                 'allegro_price' : all_price,
                 'nokaut_price' : nok_price,
-                'allegro_price_mode' : 'price win' if mode == 'a' else 'price',
-                'nokaut_price_mode' : 'price win' if mode == 'n' else 'price'}
-
-@view_config(route_name = 'not_found', renderer = 'pyramid_app:templates/not_found.mako')
-def nof_found_view(request):
-    return {}
-
-def _get_prices(search_phrase):
-    nokaut_key = 'a8839b1180ea00fa1cf7c6b74ca01bb5'
-
-    try :
-        all = allegro_api(search_phrase)
-        nok = nokaut_api(search_phrase,nokaut_key)
-    except NoItemException:
-        return (None, None), (None, None), False
-    nok_price = nok[1]
-    all_price = all[1]
-
-    if all_price < nok_price:
-        mode = 'a'
-    else:
-        mode = 'n'
-
-    return all, nok, mode
+                'allegro_price_mode' : all_mode,
+                'nokaut_price_mode' : nok_mode
+        }
